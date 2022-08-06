67c20c975032cbb4ab45f072c8201dc64088211b
M-Fakhr1 <juitar120@gmail.com>
2022-08-06 16:54:08 +0700

    Projet Pertama

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+
+class Post 
+{
+    static $blog_posts = [
+        [
+            "title" => "Judul Tulisan Pertama",
+            "slug" => "Judul Tulisan Pertama",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam autem aut asperiores quasi, reprehenderit sit minus error minima nesciunt repellendus amet, distinctio quos, tempore voluptatem magnam hic eius voluptatum est?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet dolore consequatur velit voluptas ducimus incidunt consectetur porro, blanditiis deserunt repellat laboriosam distinctio quia harum facere aut, cumque commodi mollitia corporis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, optio fuga facilis in aliquid, illum voluptates consequuntur dolores repudiandae laudantium, sint soluta! Illo amet a ducimus similique quasi eius ipsum."
+        ]
+    ];
+}
